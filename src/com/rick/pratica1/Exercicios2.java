08df40df01e59702ddac221bd82fa8eea8704279
Rich Elton <richelton14@gmail.com>
2018-04-30 23:55:19 -0300

    Questão 1 e 2 resolvidas

---
+++ src/com/rick/pratica1/Exercicios2.java
@@ -0,0 +1,70 @@
+package com.rick.pratica1;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+/**
+ * Classe responsável por resolver a questão 2(1º lista de exerícios praticas) da disciplina
+ * de teoria dos grafos.
+ * 
+ * @author Rich Elton - 117210710
+ *
+ */
+public class Exercicios2 {
+	
+	/**
+	 * Método principal.
+	 * 
+	 * @param args : Nunca é usada.
+	 */
+	public static void main(String[] args) {
+		
+		/**
+		 * Crio o grafo ponderado.
+		 */
+		SimpleWeightedGraph<String, DefaultWeightedEdge> grafo = criandoGrafo();
+		
+		/**
+		 * Algoritmo que retorna o menor caminho.
+		 */
+		DijkstraShortestPath<String, DefaultWeightedEdge> path = new DijkstraShortestPath<>(grafo);
+		GraphPath<String, DefaultWeightedEdge> menorCaminho = path.getPath("a", "d");
+		
+		System.out.println("Menor Caminho: " + menorCaminho.toString());
+		System.out.println("Quantidade de arestas: " + menorCaminho.getLength());
+		System.out.println("Valor caminho: " + menorCaminho.getWeight());
+	}
+
+	/**
+	 * Método que cria o grafo correspondente no exercício 2.
+	 * 
+	 * @return : Um grafo ponderado(SimpleWeightedGraph<String, DefaultWeightedEdge>).
+	 */
+	private static SimpleWeightedGraph<String, DefaultWeightedEdge> criandoGrafo() {
+		
+		SimpleWeightedGraph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		
+		g.addVertex("a");	g.addVertex("b");	g.addVertex("c");
+		g.addVertex("d");	g.addVertex("e");	g.addVertex("f");
+		g.addVertex("g");	g.addVertex("h");	g.addVertex("i");
+		
+		g.setEdgeWeight(g.addEdge("a","b"),2);
+		g.setEdgeWeight(g.addEdge("b","c"),4);
+		g.setEdgeWeight(g.addEdge("d","e"),1);
+		g.setEdgeWeight(g.addEdge("e","f"),6);
+		g.setEdgeWeight(g.addEdge("f","a"),7);
+		g.setEdgeWeight(g.addEdge("a","g"),3);
+		g.setEdgeWeight(g.addEdge("c","d"),2);
+		g.setEdgeWeight(g.addEdge("g","i"),1);
+		g.setEdgeWeight(g.addEdge("g","b"),6);
+		g.setEdgeWeight(g.addEdge("h","c"),2);
+		g.setEdgeWeight(g.addEdge("g","h"),3);
+		g.setEdgeWeight(g.addEdge("h","i"),4);
+		g.setEdgeWeight(g.addEdge("i","f"),5);
+		g.setEdgeWeight(g.addEdge("h","d"),8);
+		g.setEdgeWeight(g.addEdge("i","e"),2);
+		return g;
+	}
+}
---
+++ src/com/rick/pratica1/Exericios1.java
@@ -0,0 +1,142 @@
+package com.rick.pratica1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.SimpleGraph;
+
+/**
+ * Classe responsável pela resolução da questão 1(1º lista de exerícios praticas) da disciplina
+ * Teoria dos grafos.
+ * 
+ * @author Rich Elton - 117210710
+ *
+ */
+public class Exericios1 {
+
+	/**
+	 * Método principal.
+	 * 
+	 * @param args : Nunca é usado.
+	 */
+	public static void main(String[] args) {
+		
+		/**
+		 * Chamo a função que vai criar e retornar meu grafo.
+		 */
+		Graph<String, DefaultEdge> grafo = criarGrafo();
+		
+		/**
+		 * Chama a função que vai procurar minha matriz de incidência.
+		 */
+		achaMatrizIncidencia(grafo);
+	}
+	
+	/**
+	 * Método que vai achar minha matriz incidência de um grafo.
+	 * 
+	 * @param g : Grafo na qual eu quero achar a matriz de incidência.
+	 */
+	private static void achaMatrizIncidencia(Graph<String, DefaultEdge> g) {
+		
+		/**
+		 * Lista de arestas.
+		 */
+		Set<DefaultEdge> arestas = g.edgeSet();
+		
+		/**
+		 * Lista dos vertices.
+		 */
+		Set<String> vertices = g.vertexSet();
+		
+		
+		List<String> listaVertices = new ArrayList<String>();
+		Iterator<String> it = vertices.iterator();
+		
+		/**
+		 * Pego todos os meus vertices e coloco dentro da minha listaVertices.
+		 */
+		while(it.hasNext()) {
+			listaVertices.add(it.next());
+		}
+		
+		Iterator<DefaultEdge> iterator = arestas.iterator();
+		Collections.sort(listaVertices);
+		
+		/**
+		 * Função que vai imprimir minha matriz.
+		 */
+		constroiMatriz(g, listaVertices, iterator);
+		
+	}
+	
+	/**
+	 * Método que vai imprimir minha matriz de incidência.
+	 * @param g : Graph<String, DefaultEdge>.
+	 * @param listaVertices : List<String>.
+	 * @param iterator : Iterator<DefaultEdge>.
+	 */
+	private static void constroiMatriz(Graph<String, DefaultEdge> g, List<String> listaVertices,
+			Iterator<DefaultEdge> iterator) {
+		
+		List<DefaultEdge> arestas = new ArrayList<DefaultEdge>();
+		/**
+		 * Adiciono todas as arestas do meu grafo.
+		 */
+		while(iterator.hasNext()) {
+			arestas.add(iterator.next());
+		}
+		
+		/**
+		 * Imprimo o inicio e o fim de cada aresta do meu grafo.
+		 */
+		for(DefaultEdge aresta : g.edgeSet()) {
+			String inicio = g.getEdgeSource(aresta);
+			String fim = g.getEdgeTarget(aresta);
+			System.out.print("  |" + inicio+fim);
+		}
+		System.out.println("");
+		
+		/**
+		 * Pego cada vertice e cada aresta(inicio e fim) e comparo se um dos terminais da
+		 * aresta é igual ao meu vertice.
+		 * Se for, imprimo "1", caso contrário imprimo "0".
+		 */
+		for(String v : listaVertices) {
+			System.out.print(v + " |");
+			for(DefaultEdge aresta : arestas) {
+				String inicio = g.getEdgeSource(aresta);
+				String fim = g.getEdgeTarget(aresta);
+				if(inicio.equals(v) || fim.equals(v)) {
+					System.out.print(" 1  |");
+				} else {
+					System.out.print(" 0  |");
+				}
+			}
+			System.out.println("");
+		}
+	}
+
+	/**
+	 * Método que irá criar um grafo.
+	 * 
+	 * @return : Um grafo(Graph<String, DefaultEdge>)
+	 */
+	public static Graph<String, DefaultEdge> criarGrafo(){
+		
+		Graph<String, DefaultEdge> grafo = new SimpleGraph<>(DefaultEdge.class);
+		
+		grafo.addVertex("a");	grafo.addVertex("b");	grafo.addVertex("c");
+		grafo.addVertex("d");	grafo.addVertex("e");   grafo.addVertex("f");
+		
+		grafo.addEdge("a", "b");	grafo.addEdge("a", "b");
+		grafo.addEdge("b", "c");	grafo.addEdge("b", "d");	grafo.addEdge("b", "e");
+		grafo.addEdge("c", "e");	grafo.addEdge("c", "d");
+		grafo.addEdge("e", "d");	grafo.addEdge("d", "f");	grafo.addEdge("e", "f");
+		return grafo;
+	}
+}
