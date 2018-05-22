2d0216b37827ba209fec4868525523fc4c2c3ebb
Rich Elton <richelton14@gmail.com>
2018-05-21 22:45:18 -0300

    Prática 2(Quase concluída)

--- src/com/rick/pratica2/Exercicio2.java
+++ src/com/rick/pratica2/Exercicio2.java
@@ -1,12 +1,8 @@
 package com.rick.pratica2;
 
-import org.jgrapht.alg.ConnectivityInspector;
-import org.jgrapht.alg.cycle.PatonCycleBase;
-
+import org.jgrapht.alg.cycle.HierholzerEulerianCycle;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
-
+import java.util.List;
 import org.jgrapht.Graph;
 
 public class Exercicio2 {
@@ -21,28 +17,63 @@ public class Exercicio2 {
 				break;
 			}
 		}
-		System.out.println(verticePonte(grafo, vInicio));
-	}
-	
-	public static boolean verticePonte(Graph<Object, RelationshipEdge> grafo, Object vertice) {
-		PatonCycleBase cb = new PatonCycleBase<>(grafo);
+		HierholzerEulerianCycle<Object, RelationshipEdge> hz = new HierholzerEulerianCycle<>();
+		List<Object> listaVertices = hz.getEulerianCycle(grafo).getVertexList();
+		List<Object> circuito = listaVertices;
 		
-		// 	outgoingEdgesOf()--> VErificar
+		int indiceInicial = 0;
+		for (int i = 0; i < circuito.size(); i++) {
+			if (vInicio.equals(circuito.get(i))) {
+				indiceInicial = i;
+				break;
+			}
+		}
+		
+		ArrayList<Object> caminho = new ArrayList<Object>();
+		int a = circuito.size();
+		while (a >= 0) {
+			if (indiceInicial == circuito.size()) {
+				indiceInicial = 0;
+			} else {
+				caminho.add(circuito.get(indiceInicial));
+			}
+			indiceInicial++;
+			a--;
+		}
+		System.out.println(caminho);
+	}
+	/*public static boolean arestaPonte(Graph<Object, RelationshipEdge> grafo, RelationshipEdge aresta) {
+		PatonCycleBase cb = new PatonCycleBase<>(grafo);
+
 		ArrayList<ArrayList<Object>> ciclos = (ArrayList<ArrayList<Object>>) cb.findCycleBase();
 		for(ArrayList<Object> lista : ciclos) {
-			for(Object a : lista) {
-				if (a.equals(vertice)) {
-					System.out.println(lista);
-					System.out.println("aqui");
-					return true;
+			for(int i = 0; i < lista.size(); i++) {
+				RelationshipEdge a = null;
+				if (i != lista.size() - 1) {
+					a = grafo.getEdge(lista.get(i), lista.get(i+1));
+				}
+				if (a != null && aresta != null) {
+					if (aresta.equals(a)) {
+						return false;
+					}
 				}
 			}
 		}
-		return false;
+		return true;
 	}
 	
-	public static void arestaIncidente(Graph<Object, RelationshipEdge> g) {
-		
+	public static RelationshipEdge arestaIncidente(Graph<Object, RelationshipEdge> g, Object v) {
+		Set<RelationshipEdge> arestas = g.edgesOf(v);
+		Object fim = null;
+		for(RelationshipEdge a : arestas) {
+			if (!a.getV1().equals(v)) {
+				fim = a.getV1();
+			} else {
+				fim = a.getV2();
+			}
+		}
+		RelationshipEdge aresta = g.getEdge(v, fim);
+		return aresta;
 	}
 	
 	public static ArrayList<RelationshipEdge> algFleury(Graph<Object, RelationshipEdge> grafo, Object v){		
@@ -57,19 +88,22 @@ public class Exercicio2 {
 		Set<RelationshipEdge> arestas = gCopia.edgeSet();
 		Iterator<RelationshipEdge> it = arestas.iterator();
 		
-		
 		while (it.hasNext()) {
 			Object vi = (Object) verticesUsados.get(verticesUsados.size()-1);
+			RelationshipEdge ai = null;
 			if (gCopia.inDegreeOf(vi) == 1) {
-				Set<RelationshipEdge> ai = gCopia.edgesOf(vi);
+				ai = arestaIncidente(gCopia, vi);
 			} else {
-				if (!verticePonte(grafo, vi)) {
-					
+				RelationshipEdge a = arestaIncidente(gCopia, vi);
+				if(!arestaPonte(grafo, a)) {
+					ai = a;
 				}
 			}
+			gCopia.removeEdge(ai);
+			verticesUsados.add(ai);
+			Object vj = grafo.getEdgeSource(ai);
+			verticesUsados.add(vj);
 		}
-		
-		return saida;
-	}
-	
+		return verticesUsados;
+	}*/	
 }
