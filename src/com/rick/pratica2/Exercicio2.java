7218f5f8d150b4a47639286fc20c68d0e6083955
Rick <richelton14@gmail.com>
2018-06-06 10:38:11 -0300

    Escrevendo a saida em arquivo de texto e separando o programa em métodos

--- src/com/rick/pratica2/Exercicio2.java
+++ src/com/rick/pratica2/Exercicio2.java
@@ -1,26 +1,57 @@
 package com.rick.pratica2;
 
 import org.jgrapht.alg.cycle.HierholzerEulerianCycle;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.jgrapht.Graph;
 
 public class Exercicio2 {
 	
-	public static void main(String[] args) {
+	private static HierholzerEulerianCycle<Object, RelationshipEdge> hz;
+	private static BufferedWriter principal;
+	
+	public static void main(String[] args) throws IOException {
+		
+		// Criando arquivo de escrita(txt).
+		String caminho = new File("").getAbsolutePath()+"/arquivos/pratica2-ex2.txt";
+		principal = new BufferedWriter(new FileWriter(caminho));
+		principal.write("Questão 2 da pratica 2 - Teoria dos Grafos.\n");
+		
+		hz = new HierholzerEulerianCycle<>();
 		Graph<Object, RelationshipEdge> grafo = ImportGraphGML.importaGrafo();
 		
+		if(hz.isEulerian(grafo)) {
+			achaCicloEuler(grafo, "C");
+		} else {
+			String res = "Grafo não possui um ciclo de Euler.";
+			principal.write(res);
+		}
+		principal.close();
+	}
+	
+	/**
+	 * Função que acha o ciclo Euler de um grafo saindo de um determinado ponto.
+	 * @param grafo Grafo na qual você quer achar o ciclo.
+	 * @param vertice O vertice inicial.
+	 */
+	public static void achaCicloEuler(Graph<Object, RelationshipEdge> grafo, String vertice) throws IOException {
+		
+		// Acho o vertice.
 		Object vInicio = null;
 		for(Object a : grafo.vertexSet()) {
-			if(a.toString().equals("C")) {
+			if(a.toString().equals(vertice)) {
 				vInicio = a;
 				break;
 			}
 		}
-		HierholzerEulerianCycle<Object, RelationshipEdge> hz = new HierholzerEulerianCycle<>();
+		
+		// Acho o caminho de vertices do meu ciclo de Euler.
 		List<Object> listaVertices = hz.getEulerianCycle(grafo).getVertexList();
 		List<Object> circuito = listaVertices;
 		
+		// Procuro pelo vertice que desejo iniciar.
 		int indiceInicial = 0;
 		for (int i = 0; i < circuito.size(); i++) {
 			if (vInicio.equals(circuito.get(i))) {
@@ -29,6 +60,7 @@ public class Exercicio2 {
 			}
 		}
 		
+		// Faço o caminho do ciclo de Euler a partir do meu vertice inical.
 		ArrayList<Object> caminho = new ArrayList<Object>();
 		int a = circuito.size();
 		while (a >= 0) {
@@ -41,69 +73,6 @@ public class Exercicio2 {
 			a--;
 		}
 		System.out.println(caminho);
+		principal.write("Caminho: " + caminho.toString());
 	}
-	/*public static boolean arestaPonte(Graph<Object, RelationshipEdge> grafo, RelationshipEdge aresta) {
-		PatonCycleBase cb = new PatonCycleBase<>(grafo);
-
-		ArrayList<ArrayList<Object>> ciclos = (ArrayList<ArrayList<Object>>) cb.findCycleBase();
-		for(ArrayList<Object> lista : ciclos) {
-			for(int i = 0; i < lista.size(); i++) {
-				RelationshipEdge a = null;
-				if (i != lista.size() - 1) {
-					a = grafo.getEdge(lista.get(i), lista.get(i+1));
-				}
-				if (a != null && aresta != null) {
-					if (aresta.equals(a)) {
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
-	
-	public static RelationshipEdge arestaIncidente(Graph<Object, RelationshipEdge> g, Object v) {
-		Set<RelationshipEdge> arestas = g.edgesOf(v);
-		Object fim = null;
-		for(RelationshipEdge a : arestas) {
-			if (!a.getV1().equals(v)) {
-				fim = a.getV1();
-			} else {
-				fim = a.getV2();
-			}
-		}
-		RelationshipEdge aresta = g.getEdge(v, fim);
-		return aresta;
-	}
-	
-	public static ArrayList<RelationshipEdge> algFleury(Graph<Object, RelationshipEdge> grafo, Object v){		
-		ArrayList<RelationshipEdge> saida = new ArrayList<>();
-		ArrayList verticesUsados = new ArrayList();
-		
-		Graph<Object, RelationshipEdge> gCopia = grafo;
-		Object vertice = v;
-		verticesUsados.add(vertice);
-
-		ConnectivityInspector<Object, RelationshipEdge> c = new ConnectivityInspector<Object, RelationshipEdge>(gCopia);
-		Set<RelationshipEdge> arestas = gCopia.edgeSet();
-		Iterator<RelationshipEdge> it = arestas.iterator();
-		
-		while (it.hasNext()) {
-			Object vi = (Object) verticesUsados.get(verticesUsados.size()-1);
-			RelationshipEdge ai = null;
-			if (gCopia.inDegreeOf(vi) == 1) {
-				ai = arestaIncidente(gCopia, vi);
-			} else {
-				RelationshipEdge a = arestaIncidente(gCopia, vi);
-				if(!arestaPonte(grafo, a)) {
-					ai = a;
-				}
-			}
-			gCopia.removeEdge(ai);
-			verticesUsados.add(ai);
-			Object vj = grafo.getEdgeSource(ai);
-			verticesUsados.add(vj);
-		}
-		return verticesUsados;
-	}*/	
 }
