656647980ed01e5f9f4c0d3bf23f395fb9b30aea
Rich Elton <richelton14@gmail.com>
2018-07-10 20:12:56 -0300

    Pratica 3 concluida

--- src/com/rick/pratica3/Questao2.java
+++ src/com/rick/pratica3/Questao2.java
@@ -1,10 +1,13 @@
 package com.rick.pratica3;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Questao2 {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		String c = new File("").getAbsolutePath()+"/src/com/rick/pratica3/teia.gml";
 		MetricasDoGrafo metricas = new MetricasDoGrafo(c);
 		
